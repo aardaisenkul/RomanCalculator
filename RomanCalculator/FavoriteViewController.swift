44f1acee7ca9058e8233e6ed94d92aa29f6ac5de
Ali Arda İsenkul <34447040+aardaisenkul@users.noreply.github.com>
2022-01-09 15:47:29 +0300

    Calculator Oluşturuldu

---
+++ RomanCalculator/FavoriteViewController.swift
@@ -0,0 +1,123 @@
+//
+//  FavoriteViewController.swift
+//  RomanCalculator
+//
+//  Created by Ali Arda İsenkul on 9.01.2022.
+//
+
+import UIKit
+import CoreData
+
+class FavoriteViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    var fetchData = [String]()
+    var idArray = [UUID]()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = fetchData[indexPath.row]
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+        getData()
+    }
+    
+    func getData() {
+        fetchData.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favorites")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let roman = result.value(forKey: "roman") as? String,
+                       let number = result.value(forKey: "number") as? Int32 {
+                        self.fetchData.append(roman + " - \(number)")
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+            
+            
+        } catch {
+            print("error")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favorites")
+            
+            let idString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let id = result.value(forKey: "id") as? UUID {
+                            
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                fetchData.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                    
+                                } catch {
+                                    print("error")
+                                }
+                                
+                                break
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                    
+                    
+                }
+            } catch {
+                print("error")
+            }
+        }
+        
+    }
+    
+}
+
+
+
---
+++ RomanCalculator/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  RomanCalculator
+//
+//  Created by Ali Arda İsenkul on 9.01.2022.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    var romanNumbers = ["I": 1,
+                            "V": 5,
+                            "X": 10,
+                            "L": 50,
+                            "C": 100,
+                            "D": 500,
+                            "M": 1000]
+
+    @IBOutlet weak var romanNumber: UITextField!
+    @IBOutlet weak var resultNumber: UITextField!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    // Sol -
+    // Sağ +
+    // ARDIŞIK KULLANILIR
+    // SOLDAKİ KÜÇÜKSE ÇIKARILIR XIV 10-1+5 XVI 10+5+1
+    // BİR SEMBOL 3 DEN FAZLA TEKRARLANMAZ
+    //V, L ve D sembolleri asla tekrarlanmaz.
+    //Bir sembol üç defadan fazla tekrarlanmaz.
+  //  Daha büyük değerli bir sembolün sağına daha küçük bir sembol yazılırsa, değeri daha büyük sembolün değerine eklenir. Örneğin, VI=5+1=6, XI=11 vb.
+  //  Daha büyük değerli bir sembolün soluna daha küçük bir sembol yazılırsa, değeri büyük sembolün değerinden çıkarılır. IV= 5-1=4, IX=9 vb.
+ //    V, L ve D sembolleri hiçbir zaman daha büyük değere sahip bir sembolün soluna yazılmaz, yani V, L ve D asla çıkarılmaz. I sembolü sadece V ve X'ten çıkarılabilir. X sembolü sadece L, M ve C'den çıkarılabilir.
+  //  Bir sembol, daha büyük değere sahip belirli bir sembolden bir kereden fazla çıkarılamaz. Başka bir deyişle, bir sembolün sol tarafında bir sembolü tekrarlayamayız. Örneğin, 98 IIC olarak değil XCVIII olarak yazılmıştır.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.bookmarks, target: self, action: #selector(goToFavorites))
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        romanNumber.text = ""
+        resultNumber.text = ""
+        favoriteButton.isEnabled = false
+    }
+   
+    
+    @objc func goToFavorites(){
+        performSegue(withIdentifier: "showFavorites", sender: nil)
+    }
+
+    @IBAction func addToFavorite(_ sender: Any) {
+        
+        guard Int(resultNumber.text!) != nil
+        else {
+            print("hata")
+           return
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newFavorite = NSEntityDescription.insertNewObject(forEntityName: "Favorites", into: context)
+        
+        //Attributes
+        
+        newFavorite.setValue(romanNumber.text!, forKey: "roman")
+        newFavorite.setValue(Int32(resultNumber.text!), forKey: "number")
+        
+        newFavorite.setValue(UUID(), forKey: "id")
+        
+        
+        do {
+            try context.save()
+            print("success")
+        } catch {
+            print("error")
+        }
+        
+      
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        goToFavorites()
+    }
+    @IBAction func convertNumber(_ sender: Any) {
+       let romanN = romanNumber.text ?? ""
+        if romanN == "" {
+            resultNumber.text = String("INVALID INPUT")
+            return
+        }
+        
+        let romanArray = Array(romanN.uppercased())
+       
+        if !checkNumbers(romanArray: romanArray) {
+            resultNumber.text = String("INVALID INPUT")
+            return
+        }
+        
+        if !checkOccurence(charArray: romanArray) {
+            resultNumber.text = String("INVALID INPUT")
+            return
+        }
+        let convertResult = romanToNumbers(charArray: romanArray)
+
+        resultNumber.text = String(convertResult)
+        favoriteButton.isEnabled = true
+    }
+    func romanToNumbers(charArray:Array<Character>) -> Int {
+        
+        var index = 0
+        var allNumbers = [Int]()
+        
+        while(index < charArray.count) {
+            var first = romanNumbers[String(charArray[index])]! // I - 1
+            var second = 0
+            if index < charArray.count - 1 {
+                second = romanNumbers[String(charArray[index + 1])]! // V - 5 VII
+                print(first, second)
+                if second > first {
+                    first = second - first
+                    index += 1
+                }
+            }
+            allNumbers.append(first)
+            index += 1
+        }
+        var total = 0
+        for item in allNumbers {
+            total += item
+        }
+        return total
+    }
+    func checkNumbers(romanArray:Array<Character>) -> Bool {
+        for item in romanArray {
+            if romanNumbers[String(item)] == nil {
+                return false
+            }
+            let filterArray = romanArray.filter { char in
+                return char == item
+            }
+            if item == "L" || item == "V" || item == "D"{
+                if filterArray.count > 1 {
+                    return false
+                }
+            }else {
+                if(filterArray.count > 3) {
+                   return false
+               }
+            }
+        }
+        return true
+    }
+    func checkOccurence(charArray:Array<Character>) -> Bool {
+        var index = 0
+        while(index < charArray.count) {
+            if index == charArray.count - 1 {
+                break
+            }
+        if charArray[index] == "X"{
+            if charArray[index+1] == "D" || charArray[index+1] == "M"{
+                return false
+            }
+        }else if charArray[index] == "L"{
+            if charArray[index+1] == "C" || charArray[index+1] == "D" || charArray[index+1] == "M"{
+                return false
+            }
+        }
+        else if charArray[index] == "D"{
+            if  charArray[index+1] == "M"{
+                return false
+            }
+        }
+            index+=1
+    }
+        return true
+    }
+    
+}
+
